816098945e2ee8940427a23f1c3e4adb5f90768c
jonvgray <135182489+jonvgray@users.noreply.github.com>
2025-02-27 23:37:52 -0800

    Saving changes before rebase

--- .ipynb_checkpoints/Auth-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-# Authenticate
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()  # Opens a browser to sign in
-
-drive = GoogleDrive(gauth)
